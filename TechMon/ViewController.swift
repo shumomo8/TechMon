38c01c9c849be46e101aab592339500afb3043ce
shu fujita <momo.8jack@gmail.com>
2020-05-15 19:23:26 +0900

    Initial Commit

---
+++ TechMon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechMon
+//
+//  Created by Shu Fujita on 2020/05/15.
+//  Copyright © 2020 Fujita shu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
